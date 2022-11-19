ca6068e1435e99370e17f5ce02a33d6812995629
Yunjiao Chai <yc886@cornell.edu>
2022-11-19 17:01:38 -0500

    Finish 3

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,68 +1,74 @@
 import React, {Component} from 'react';
 import { Row, Col } from 'antd';
-import axios from "axios";
-
+import axios from 'axios';
+import { BASE_URL, NEARBY_SATELLITE, SAT_API_KEY, STARLINK_CATEGORY } from '../constants';
 import SatSetting from './SatSetting';
 import SatelliteList from './SatelliteList';
-import { SAT_API_KEY, BASE_URL, NEARBY_SATELLITE, STARLINK_CATEGORY } from "../constants";
+import WorldMap from './WorldMap';
 
 class Main extends Component {
-    state = {
-        setting: {},
-        satInfo: {},
-        satList: [],
-        isLoadingList: false
+    constructor(){
+        super();
+        this.state = {
+            setting: {},
+            satInfo: {},
+            satList: [],
+            isLoadingList: false
+        }
+    }
+    render() {
+        const { isLoadingList, satInfo, satList, setting } = this.state;
+        return (
+            <Row className='main'>
+                <Col span={8} className="left-side">
+                    <SatSetting onShow={this.showNearbySatellite}/>
+                    <SatelliteList isLoad={isLoadingList}
+                                   satInfo={satInfo}
+                                   onShowMap={this.showMap} />
+                </Col>
+                <Col span={16} className="right-side">
+                    <WorldMap satData={satList} observerData={setting} />
+                </Col>
+            </Row>
+        );
+    }
+
+
+    showMap = (selected) => {
+        this.setState(preState => ({
+            ...preState,
+            satList: [...selected]
+        }))
     }
 
     showNearbySatellite = (setting) => {
-        console.log('show nearby')
         this.setState({
+            isLoadingList: true,
             setting: setting
         })
         this.fetchSatellite(setting);
     }
 
-    fetchSatellite = (setting) => {
-        console.log("fetching")
-        const { latitude, longitude, elevation, altitude } = setting;
+    fetchSatellite= (setting) => {
+        const {latitude, longitude, elevation, altitude} = setting;
         const url = `${BASE_URL}/${NEARBY_SATELLITE}/${latitude}/${longitude}/${elevation}/${altitude}/${STARLINK_CATEGORY}/&apiKey=${SAT_API_KEY}`;
 
-        this.setState({
-            isLoadingList: true
-        });
+   this.setState({
+       isLoadingList: true
+   });
 
-        axios.get(url)
-            .then( res => {
-                console.log(res.data);
-                this.setState({
-                    satInfo: res.data,
-                    isLoadingList: false
-                })
-            })
-            .catch( error => {
-                this.setState({
-                    isLoadingList: false
-                });
-                console.log('err in fetch satellite -> ', error);
-            })
-    }
-
-    render() {
-        const { satInfo, isLoadingList, satList, setting } = this.state;
-
-        return (
-            <Row className='main'>
-                <Col span={8} className="left-side">
-                    <SatSetting onShow={this.showNearbySatellite} />
-                    <SatelliteList satInfo={satInfo}
-                                   isLoad={isLoadingList} />
-                </Col>
-                <Col span={16} className="right-side">
-                    right
-                </Col>
-            </Row>
-        );
+   axios.get(url)
+       .then(response => {
+           console.log(response.data)
+           this.setState({
+               satInfo: response.data,
+               isLoadingList: false
+           })
+       })
+       .catch(error => {
+           console.log('err in fetch satellite -> ', error);
+       })
     }
 }
-
 export default Main;
+
--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,9 @@
-export const SAT_API_KEY = "YOUR KEY";
+export const SAT_API_KEY = "F5R63F-9QGGZJ-2YXVVW-4YGD";
 
 export const STARLINK_CATEGORY = "52";
 
 export const NEARBY_SATELLITE = "rest/v1/satellite/above";
 
 export const BASE_URL = ""; // your backend endpoint when deploy
+export const WORLD_MAP_URL = "https://unpkg.com/world-atlas@1/world/110m.json";
+export const SATELLITE_POSITION_URL = "rest/v1/satellite/positions";
